fa7a3541406b003b1f8383a585c4c6f5ead93b22
jeongjaino <akgk155@gmail.com>
2023-04-03 20:42:29 +0900

    [FEATURE] #3 : repository, app 의존성 생성 및 주입

---
+++ android/core/data/src/main/java/com/jaino/data/di/RepositoryModule.kt
@@ -0,0 +1,29 @@
+package com.jaino.data.di
+
+import com.jaino.data.repository.auth.AuthRepository
+import com.jaino.data.repository.auth.AuthRepositoryImpl
+import com.jaino.data.repository.auth.SocialAuthRepository
+import com.jaino.data.repository.auth.SocialAuthRepositoryImpl
+import com.jaino.datastore.BeJuRyuDatastore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ActivityComponent::class)
+class RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideSocialAuthRepository(
+        repositoryImpl : SocialAuthRepositoryImpl
+    ) : SocialAuthRepository = repositoryImpl
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(
+        dataStore: BeJuRyuDatastore
+    ) : AuthRepository = AuthRepositoryImpl(dataStore)
+}
