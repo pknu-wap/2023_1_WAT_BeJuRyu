de482b8617db47d40369b22dade5bb7bef9352f7
onegqueen <rkdwldnjs30@naver.com>
2023-04-10 02:53:15 +0900

    [CHORE] #9 : spring set-up

--- backend/src/main/java/com/WAT/BEJURYU/BejuryuApplication.java
+++ backend/src/main/java/com/WAT/BEJURYU/BejuryuApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BejuryuApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BejuryuApplication.class, args);
 	}
