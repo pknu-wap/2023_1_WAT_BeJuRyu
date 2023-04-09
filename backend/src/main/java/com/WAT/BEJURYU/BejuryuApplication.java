2eb953f875e12ec222b3d0459d6b75f10221bc53
onegqueen <rkdwldnjs30@naver.com>
2023-04-10 00:25:29 +0900

    스프링 프로젝트 생성

---
+++ backend/src/main/java/com/WAT/BEJURYU/BejuryuApplication.java
@@ -0,0 +1,13 @@
+package com.WAT.BEJURYU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BejuryuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BejuryuApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/WAT/BEJURYU/BejuryuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WAT.BEJURYU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BejuryuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
