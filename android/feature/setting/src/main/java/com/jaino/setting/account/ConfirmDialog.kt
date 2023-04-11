795362ca974655b130b4c4de29a026c91b421acf
jeongjaino <akgk155@gmail.com>
2023-04-11 18:17:41 +0900

    [FEATURE] #12 : expire, logout시 사용할 Confirm Dialog 구현

---
+++ android/feature/setting/src/main/java/com/jaino/setting/account/ConfirmDialog.kt
@@ -0,0 +1,43 @@
+package com.jaino.setting.account
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.WindowManager
+import com.jaino.setting.databinding.DialogConfirmBinding
+
+class ConfirmDialog  (
+    context : Context,
+    private val title : String,
+    private val onDoneButtonClick: () -> Unit
+): Dialog(context){
+
+    private val binding by lazy { DialogConfirmBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.setContentView(binding.root)
+        initViews()
+        resizeDialog()
+    }
+
+    private fun initViews(){
+        binding.dialogConfirmTextView.text = title
+
+        binding.dialogConfirmPositiveButton.setOnClickListener{
+            onDoneButtonClick()
+            dismiss()
+        }
+
+        binding.dialogConfirmNegativeButton.setOnClickListener{
+            dismiss()
+        }
+    }
+
+    private fun resizeDialog(){
+        window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+    }
+}
