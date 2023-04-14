a53743f413666514ad48979ef1c931d38d2bd3ce
jeongjaino <akgk155@gmail.com>
2023-04-15 00:17:37 +0900

    [FEATURE] #14 : 사용자 분석 정보 dto, domain model 구현

---
+++ android/core/model/src/main/java/com/jaino/model/user/Analysis.kt
@@ -0,0 +1,6 @@
+package com.jaino.model.user
+
+data class Analysis (
+    val id : Long,
+    val date: String
+    )
---
+++ android/core/model/src/main/java/com/jaino/model/user/AnalysisSentiment.kt
@@ -0,0 +1,32 @@
+package com.jaino.model.user
+
+data class AnalysisSentiment(
+        val source : AnalysisSource,
+        val result : AnalysisResult
+){
+        constructor() : this(
+                AnalysisSource("", ""),
+        AnalysisResult("", AnalysisDrink()))
+}
+
+data class AnalysisSource(
+        val textExpression: String,
+        val facialExpression: String
+)
+
+data class AnalysisResult(
+        val sentiment: String,
+        val drink: AnalysisDrink
+)
+
+data class AnalysisDrink(
+        val id: Long,
+        val name : String,
+        val dosu : Double,
+        val volume : Int,
+        val price : Int,
+        val type : String,
+        val bottle_color : String
+){
+        constructor() : this(0, "", 0.0, 0,0, "", "")
+}
