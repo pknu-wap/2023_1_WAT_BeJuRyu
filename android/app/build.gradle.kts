72e02d0d9c1881e5b6ade0279b1d5e8ca8d49608
jeongjaino <akgk155@gmail.com>
2023-04-15 20:40:14 +0900

    [CHORE] #20 : include feature analyze

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
 
     implementation(project(":feature:auth"))
     implementation(project(":feature:setting"))
+    implementation(project(":feature:analyze"))
     implementation(project(":core:model"))
     implementation(project(":core:data"))
     implementation(project(":core:domain"))
