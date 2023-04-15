4810b9b693df58dd5642a63aeb9d32af1987ff8a
jeongjaino <akgk155@gmail.com>
2023-04-15 13:35:26 +0900

    [CHORE] #20 : analyze module 추가

---
+++ android/feature/analyze/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.jaino.feature")
+    id("com.jaino.hilt")
+    alias(libs.plugins.androidx.navigation.safeargs)
+}
+
+android {
+    namespace = "com.jaino.analyze"
+
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+}
+
+dependencies {
+    implementation(project(":core:data"))
+    implementation(project(":core:domain"))
+    implementation(project(":core:model"))
+    implementation(project(":core:navigation"))
+    implementation(libs.bundles.camerax)
+    implementation(libs.timber)
+    implementation(libs.kotlin.datetime)
+    implementation(libs.androidx.fragment)
+    implementation(libs.bundles.androidx)
+    implementation(libs.androidx.lifecycle.runtime)
+    implementation(libs.androidx.lifecycle.livedata)
+}
