b3fcd4ca00aedf3caa34425022d434e10b62873a
jeongjaino <akgk155@gmail.com>
2023-04-16 15:53:03 +0900

    [CHORE] #24 : build gradle default config 삭제

--- android/core/datastore/build.gradle.kts
+++ android/core/datastore/build.gradle.kts
@@ -6,15 +6,6 @@ plugins {
 
 android {
     namespace = "com.jaino.datastore"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 26
-        targetSdk = 33
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
 }
 
 dependencies {
