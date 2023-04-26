033ed8558266b279e50d799e1fb4dcc6effe5242
onegqueen <rkdwldnjs30@naver.com>
2023-04-26 20:40:33 +0900

    [CHORE] #30 : 주석삭제

--- backend/src/main/java/com/WAT/BEJURYU/entity/Sentiment.java
+++ backend/src/main/java/com/WAT/BEJURYU/entity/Sentiment.java
@@ -2,6 +2,8 @@ package com.WAT.BEJURYU.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -17,6 +19,7 @@ import lombok.NoArgsConstructor;
 public class Sentiment {
     @Id
     @Column(name="type")
+    @Enumerated(value = EnumType.STRING)
     private SentimentType sentimentType;
 
 }
--- backend/src/main/java/com/WAT/BEJURYU/repository/UserRepository.java
+++ backend/src/main/java/com/WAT/BEJURYU/repository/UserRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/*닉네임 조회 , 정보 저장*/
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     List<User> findByNickname(String nickname);
