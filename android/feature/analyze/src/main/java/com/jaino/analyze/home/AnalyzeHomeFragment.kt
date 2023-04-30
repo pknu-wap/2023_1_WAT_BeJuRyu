019ba7f5d133e76c665e4a7f21ad011dfc5bb984
jeongjaino <akgk155@gmail.com>
2023-04-30 15:31:11 +0900

    [FEATURE] #42 : navigate 설정 및 구현

--- android/feature/analyze/src/main/java/com/jaino/analyze/home/AnalyzeHomeFragment.kt
+++ android/feature/analyze/src/main/java/com/jaino/analyze/home/AnalyzeHomeFragment.kt
@@ -9,12 +9,19 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.jaino.analyze.R
 import com.jaino.analyze.databinding.FragmentAnalyzeHomeBinding
+import com.jaino.common.navigation.AppNavigator
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class AnalyzeHomeFragment : Fragment() {
 
     private var _binding: FragmentAnalyzeHomeBinding? = null
     private val binding
         get() = requireNotNull(_binding) { "binding object is not initialized" }
+
+    @Inject
+    lateinit var appNavigator: AppNavigator
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,12 +42,28 @@ class AnalyzeHomeFragment : Fragment() {
         binding.goToAnalyzeButton.setOnClickListener{
             navigateToAnalyzeText()
         }
+
+        binding.goToSearchButton.setOnClickListener{
+            navigateToSearch()
+        }
+
+        binding.goToSettingButton.setOnClickListener{
+            navigateToSetting()
+        }
     }
 
     private fun navigateToAnalyzeText(){
         findNavController().navigate(
             AnalyzeHomeFragmentDirections.actionAnalyzeHomeFragmentToAnalyzeTextFragment()
         )
+    }
+
+    private fun navigateToSetting(){
+        startActivity(appNavigator.navigateToSetting())
+    }
+
+    private fun navigateToSearch(){
+        startActivity(appNavigator.navigateToDictionary())
     }
 
     override fun onDestroy() {
