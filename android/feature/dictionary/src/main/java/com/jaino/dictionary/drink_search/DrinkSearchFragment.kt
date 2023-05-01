d3093d4049f4000325a731fbd9d2002d34c24d1b
jeongjaino <akgk155@gmail.com>
2023-05-01 13:33:15 +0900

    [CHORE] #47 : drink 검색화면 추가 및 구현

---
+++ android/feature/dictionary/src/main/java/com/jaino/dictionary/drink_search/DrinkSearchFragment.kt
@@ -0,0 +1,76 @@
+package com.jaino.dictionary.drink_search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.chip.Chip
+import com.jaino.common.navigation.AppNavigator
+import com.jaino.dictionary.R
+import com.jaino.dictionary.databinding.FragmentDrinkSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class DrinkSearchFragment : Fragment() {
+
+    private var _binding : FragmentDrinkSearchBinding ?= null
+    private val binding get() =  requireNotNull(_binding) { "binding object is not initialized" }
+
+    @Inject
+    lateinit var navigator: AppNavigator
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_drink_search, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // initAdapter()
+        initView()
+        // observeData()
+    }
+
+    private fun initView(){
+        binding.backToAnalyzeButton.setOnClickListener{
+            startActivity(navigator.navigateToAnalyze())
+        }
+
+        binding.searchEditTextView.setOnEditorActionListener { textView, actionId, keyEvent ->
+            if(actionId == EditorInfo.IME_ACTION_SEARCH && textView.text.isNotBlank()){
+                navigateToDrinkList(
+                    textView.text.toString()
+                )
+                true
+            }
+            else false
+        }
+
+        binding.drinkChipGroup.setOnCheckedStateChangeListener{ group, checkedIds ->
+            // single selection
+            val drinkCategory = group.findViewById<Chip>(checkedIds.first()).text.toString()
+            navigateToDrinkList(drinkCategory)
+        }
+    }
+
+    private fun navigateToDrinkList(word: String){
+        val direction = DrinkSearchFragmentDirections
+            .actionDrinkSearchFragmentToDrinkListFragment(word)
+        findNavController().navigate(direction)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
