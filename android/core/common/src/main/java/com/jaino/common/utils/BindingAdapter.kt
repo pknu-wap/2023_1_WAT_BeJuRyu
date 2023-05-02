8a4020d4bd30f47666fa2963e34cb8682e23d5e0
jeongjaino <akgk155@gmail.com>
2023-05-02 15:58:30 +0900

    [CHORE] #51 : databinding을 위한 bindingadapter 구현

---
+++ android/core/common/src/main/java/com/jaino/common/utils/BindingAdapter.kt
@@ -0,0 +1,24 @@
+package com.jaino.common.utils
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+
+object BindingAdapter {
+
+    @JvmStatic
+    @BindingAdapter("app:imageUrl")
+    fun loadImage(view: ImageView, src: String?) {
+        if (src != null) {
+            Glide.with(view.context)
+                .load(src)
+                .placeholder(com.jaino.designsystem.R.drawable.img)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .apply(RequestOptions().fitCenter())
+                .error(com.jaino.designsystem.R.drawable.img)
+                .into(view)
+        }
+    }
+}
