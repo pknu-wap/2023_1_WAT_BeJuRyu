d5af4747915fa398e8c054b3a0e3d4653e2adb2c
Sun Yerim <syl1602@naver.com>
2023-05-05 01:23:20 +0900

    [STYLE]#58: 주류추천 페이지 생성

--- front/src/components/Recommend/PhotoUpload.jsx
+++ front/src/components/Recommend/PhotoUpload.jsx
@@ -2,7 +2,8 @@
     동기적 실행, 주어진 객체를 가리키는 URL을 DOMString으로 변환
     창을 닫을 때까지 유지됨. 이전에 해제하기 위해서는 메모리 누수 방지 위해서 revokeObjectURL()을 호출해야함.
     FileLeader와 달리 시간이 필요하지 않고 revoke만 잘해주면 속도가 많이 빠르다고함
-    FileLeader와 readAsDataURL을 사용하는 방법과의 차이점에 대해 공부할 것
+    FileLeader와 readAsDataURL을 사용하는 방법과의 차이점에 대해 공부할 것 
+    블로그와 유트브를 참고해여
     */
 import S from "./styled";
 
