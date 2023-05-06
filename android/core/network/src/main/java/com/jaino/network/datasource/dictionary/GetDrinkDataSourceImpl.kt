ae80f9209a79a5ed7b7cbd5e6800faec34886a8e
jeongjaino <akgk155@gmail.com>
2023-05-06 17:12:50 +0900

    [FEATURE] #59 : 주류 데이터 data source 구현

---
+++ android/core/network/src/main/java/com/jaino/network/datasource/dictionary/GetDrinkDataSourceImpl.kt
@@ -0,0 +1,16 @@
+package com.jaino.network.datasource.dictionary
+
+import com.jaino.network.model.dictionary.DrinkDataResponse
+import com.jaino.network.remote.DictionaryService
+import javax.inject.Inject
+
+class GetDrinkDataSourceImpl @Inject constructor(
+    private val service: DictionaryService
+): GetDrinkDataSource{
+    override suspend fun getDrinkById(id: Long): Result<DrinkDataResponse> =
+        runCatching {
+            service.getDrinkById(id).data
+        }.onFailure {error ->
+            Result.failure<DrinkDataResponse>(error)
+        }
+}
