e33c5dd186047b7892239a37aea68c5566af60d3
onegqueen <rkdwldnjs30@naver.com>
2023-05-18 23:38:35 +0900

    [FIX] #71 : rebase

---
+++ backend/src/main/java/com/WAT/BEJURYU/service/ReviewService.java
@@ -0,0 +1,9 @@
+package com.WAT.BEJURYU.service;
+
+import com.WAT.BEJURYU.repository.ReviewRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    private ReviewRepository reviewRepository;
+}
