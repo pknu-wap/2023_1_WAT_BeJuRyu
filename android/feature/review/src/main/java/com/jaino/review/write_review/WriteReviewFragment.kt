713c551047f272a68e41ab9329db36acc97fa597
jeongjaino <akgk155@gmail.com>
2023-05-19 19:55:40 +0900

    [FEATURE] #60 : Review 작성 화면 기능 구현

---
+++ android/feature/review/src/main/java/com/jaino/review/write_review/WriteReviewFragment.kt
@@ -0,0 +1,95 @@
+package com.jaino.review.write_review
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.jaino.common.extensions.showToast
+import com.jaino.common.navigation.AppNavigator
+import com.jaino.review.R
+import com.jaino.review.databinding.FragmentWriteReviewBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class WriteReviewFragment : Fragment(){
+
+    private var _binding : FragmentWriteReviewBinding? = null
+    private val binding get() = requireNotNull(_binding){ "binding object is not initialized" }
+
+    @Inject
+    lateinit var appNavigator: AppNavigator
+
+    private val viewModel : WriteReviewViewModel by viewModels()
+    private val args : WriteReviewFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_write_review, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        observeData()
+    }
+
+    private fun initViews(){
+        binding.backButton.setOnClickListener {
+            val direction = WriteReviewFragmentDirections.actionWriteReviewFragmentToReviewListFragment(
+                args.drinkId
+            )
+            findNavController().navigate(direction)
+        }
+
+        binding.goToHomeButton.setOnClickListener {
+            startActivity(appNavigator.navigateToDictionary())
+        }
+
+        binding.reviewPostButton.setOnClickListener {
+            viewModel.postReview(args.drinkId)
+        }
+    }
+
+    private fun observeData(){
+        viewModel.uiEvent.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+            .onEach {
+                when(it){
+                    is WriteReviewViewModel.UiEvent.Failure -> {
+                        if(it.message != null) {
+                            requireContext().showToast(it.message)
+                        }
+                    }
+
+                    is WriteReviewViewModel.UiEvent.Success -> {
+                        showDialog()
+                    }
+                }
+            }
+    }
+
+    private fun showDialog(){
+
+    }
+
+    private fun navigateDictionary(){
+
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
---
+++ android/feature/review/src/main/java/com/jaino/review/write_review/WriteReviewViewModel.kt
@@ -0,0 +1,46 @@
+package com.jaino.review.write_review
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jaino.common.extensions.toDateTime
+import com.jaino.data.repository.review.ReviewRepository
+import com.jaino.data.repository.user.LocalUserRepository
+import com.jaino.model.review.WriteReview
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WriteReviewViewModel @Inject constructor(
+    private val repository : ReviewRepository,
+    private val userRepository: LocalUserRepository
+): ViewModel() {
+
+    val reviewContent : MutableStateFlow<String> = MutableStateFlow("")
+    val ratingCount : MutableStateFlow<Float> = MutableStateFlow(0.0f)
+
+    private val _uiEvent : MutableSharedFlow<UiEvent> = MutableSharedFlow<UiEvent>()
+    val uiEvent : SharedFlow<UiEvent> get() = _uiEvent
+
+    fun postReview(drinkId : Long){
+        viewModelScope.launch {
+            repository.postReview(drinkId,
+                WriteReview(userRepository.getUserId(), reviewContent.value,
+                    ratingCount.value.toInt(), System.currentTimeMillis().toDateTime()))
+                .onSuccess {
+                    _uiEvent.emit(UiEvent.Success)
+                }
+                .onFailure {
+                    _uiEvent.emit(UiEvent.Failure(it.message))
+                }
+        }
+    }
+
+    sealed class UiEvent{
+        data class Failure(val message : String?) : UiEvent()
+        object Success : UiEvent()
+    }
+}
