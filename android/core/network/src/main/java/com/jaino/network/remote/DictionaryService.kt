93abfbfd47b1b9d9763069f64de54075ecc48508
jeongjaino <akgk155@gmail.com>
2023-05-19 19:58:29 +0900

    [CHORE] #60 : model package 변경

--- android/core/network/src/main/java/com/jaino/network/remote/DictionaryService.kt
+++ android/core/network/src/main/java/com/jaino/network/remote/DictionaryService.kt
@@ -1,8 +1,8 @@
 package com.jaino.network.remote
 
-import com.jaino.network.model.base.BaseResponse
-import com.jaino.network.model.dictionary.DrinkDataResponse
-import com.jaino.network.model.dictionary.DrinkListResponse
+import com.jaino.network.model.response.base.BaseResponse
+import com.jaino.network.model.response.dictionary.DrinkDataResponse
+import com.jaino.network.model.response.dictionary.DrinkListResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,17 +11,17 @@ interface DictionaryService {
 
     // 전체 주류 리스트 조회
     @GET("/drinks/all")
-    fun getDrinkList(): DrinkListResponse
+    suspend fun getDrinkList(): DrinkListResponse
 
     // type 으로 주류 조회
     @GET("/drinks/{type}")
-    fun getDrinkListByType(
+    suspend fun getDrinkListByType(
         @Query("type") type: String
     ): DrinkListResponse
 
     // id 값으로 주류 조회
     @GET("/drinks/{drink-id}")
-    fun getDrinkById(
+    suspend fun getDrinkById(
         @Query("drink-id") id: Long
     ): BaseResponse<DrinkDataResponse>
 }
