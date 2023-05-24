df49b6150147198d6e06cc55ed42bf8581f239f0
onegqueen <rkdwldnjs30@naver.com>
2023-05-24 21:59:34 +0900

    [FEATURE] #83 : analysisApiController 작성
    자신의 분석 목록 조회 , 해당분석조회 , 분석요청(소스등록)

---
+++ backend/src/main/java/com/WAT/BEJURYU/controller/AnalysisApiControlelr.java
@@ -0,0 +1,47 @@
+package com.WAT.BEJURYU.controller;
+
+import com.WAT.BEJURYU.dto.AnalysisResponse;
+import com.WAT.BEJURYU.dto.AnalysisResponses;
+import com.WAT.BEJURYU.dto.AnalysisSourceRequest;
+import com.WAT.BEJURYU.service.AnalysisService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/analyze")
+public class AnalysisApiControlelr {
+
+    private final AnalysisService analysisService;
+
+    @GetMapping("/{analysis_id}")
+    public ResponseEntity<AnalysisResponse> findAnalysis(
+        @PathVariable(value = "analysis_id") Long analysisId) {
+        final AnalysisResponse analysis = analysisService.getAnalysis(analysisId);
+
+        return ResponseEntity.ok(analysis);
+    }
+
+    @GetMapping("/{user_id}")
+    public ResponseEntity<AnalysisResponses> findAnalyzeByUserId(
+        @PathVariable(value = "user_id") Long userId) {
+        final AnalysisResponses analyze = analysisService.getAnalyze(userId);
+
+        return ResponseEntity.ok(analyze);
+    }
+
+    @PostMapping("/source")
+    public ResponseEntity<AnalysisResponse> createAnalysisSource(
+        @RequestBody AnalysisSourceRequest sourceRequest) {
+        final AnalysisResponse analysis = analysisService.postAnalysis(sourceRequest);
+
+        return ResponseEntity.ok(analysis);
+    }
+
+}
--- backend/src/main/java/com/WAT/BEJURYU/dto/AnalysisResponses.java
+++ backend/src/main/java/com/WAT/BEJURYU/dto/AnalysisResponses.java
@@ -1,7 +1,6 @@
 package com.WAT.BEJURYU.dto;
 
 import com.WAT.BEJURYU.entity.Analysis;
-import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,11 +9,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public class AnalysisResponses {
 
-    private final List<LocalDateTime> analysisDate;
+    private final List<AnalysisResponse> analyze;
 
     public static AnalysisResponses of(final List<Analysis> analyze) {
-        final List<LocalDateTime> result = analyze.stream()
-            .map(d -> d.getDate())
+        final List<AnalysisResponse> result = analyze.stream()
+            .map(AnalysisResponse::from)
             .toList();
 
         return new AnalysisResponses(result);
