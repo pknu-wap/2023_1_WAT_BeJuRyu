9b668291c49f577b5fc53f681e8227a2c2a65923
Sun Yerim <syl1602@naver.com>
2023-05-31 02:41:02 +0900

    [FEATURE]#90: 주류 추천에 필요한 데이터 post 요청 추가 코드

--- front/src/components/Recommend/Recommend.jsx
+++ front/src/components/Recommend/Recommend.jsx
@@ -1,12 +1,13 @@
 /* TODO
 1. 거의 완료
-2. [] 이미지 첨부 후 서버와 통신할 때 URL로 넘기는 방법 찾아볼 것.(facial-expression: string
+2. [X] 이미지 첨부 후 서버와 통신할 때 json로 넘기는 방법 찾아볼 것.(facial-expression: string => base64 인코딩
 3. [X] 현재 줄글 작성 부분과 이미지 들어가는 게 따로 구현되있는데, 서버 넘길때 한 번에 넘겨야되므로 코드 합침 필요
   => formData*/
 import S from "./styled";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PhotoUpload from "./PhotoUpload";
+import authClient from "../../apis/authClient";
 
 function Recommend() {
   const navigate = useNavigate();
@@ -15,16 +16,38 @@ function Recommend() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [inputValue, setInputValue] = useState(""); // 텍스트 입력값 상태 추가
 
-  const handleFormSubmit = (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
-
-    const formData = new FormData();
-    formData.append("mood", inputValue);
-    formData.append("image", selectedFile);
-
-    // 서버와의 통신 들어갈 부분
-
     navigate("/result");
+    if (selectedFile) {
+      const reader = new FileReader();
+
+      reader.onloadend = async () => {
+        const base64Data = reader.result.split(",")[1];
+
+        const requestData = {
+          "user-id": localStorage.getItem("user-id"),
+          "text-expression": inputValue,
+          "facial-expression": base64Data,
+        };
+
+        try {
+          const res = await authClient({
+            method: "post",
+            url: "/analyze/source",
+            data: requestData,
+          });
+          console.log(res);
+          // 서버 응답 처리
+        } catch (error) {
+          const err = error.response.data;
+          console.log(err);
+          // 에러 처리
+        }
+      };
+
+      reader.readAsDataURL(selectedFile); // 이미지 파일을 Base64로 인코딩
+    }
   };
 
   const handleInputChange = (e) => {
