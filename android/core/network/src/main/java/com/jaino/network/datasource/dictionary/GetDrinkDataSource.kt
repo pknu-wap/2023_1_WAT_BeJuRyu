a269f850d3289401af114a43c671c12e7b24a2fe
jeongjaino <akgk155@gmail.com>
2023-06-01 13:03:10 +0900

    [FEATURE] #93 : DrinkDataSource 통합

--- android/core/network/src/main/java/com/jaino/network/datasource/dictionary/GetDrinkDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.jaino.network.datasource.dictionary
-
-import com.jaino.network.model.response.dictionary.DrinkInfoResponse
-
-interface GetDrinkDataSource {
-
-    suspend fun getDrinkById(id : Long): Result<DrinkInfoResponse>
-
-}
