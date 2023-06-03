326f10741acec49a0fc3ecc7113a9425b4d95d21
onegqueen <rkdwldnjs30@naver.com>
2023-06-04 01:33:11 +0900

    [FIX]

--- backend/src/main/java/com/WAT/BEJURYU/service/MemberService.java
+++ backend/src/main/java/com/WAT/BEJURYU/service/MemberService.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 @Service
-@Transactional(readOnly = true)
+@Transactional
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
