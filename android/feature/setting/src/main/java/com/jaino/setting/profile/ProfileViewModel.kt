dc8d4bd265fe21a074755781f581c508530158dc
jeongjaino <akgk155@gmail.com>
2023-06-04 01:54:46 +0900

    [FEATURE] #123 : 닉네임 불러오기 및 state 설정

--- android/feature/setting/src/main/java/com/jaino/setting/profile/ProfileViewModel.kt
+++ android/feature/setting/src/main/java/com/jaino/setting/profile/ProfileViewModel.kt
@@ -2,36 +2,54 @@ package com.jaino.setting.profile
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jaino.data.repository.setting.ProfileRepository
 import com.jaino.data.repository.user.LocalUserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-    private val repository : LocalUserRepository,
+    private val localRepository: LocalUserRepository,
+    private val repository: ProfileRepository
 ): ViewModel(){
 
-    private val _nickNameState = MutableStateFlow<String>("")
+    private val _nickNameState : MutableStateFlow<String> = MutableStateFlow<String>("")
     val nickNameState : StateFlow<String> get() = _nickNameState
 
-    init{
-        getNickName()
-    }
+    private val _profileUiEvent : MutableSharedFlow<UiEvent> = MutableSharedFlow<UiEvent>()
+    val profileUiEvent : SharedFlow<UiEvent> get() = _profileUiEvent
 
-    private fun getNickName(){
+    fun getNickName(){
         viewModelScope.launch {
-            _nickNameState.value = repository.getNickName()
-            _nickNameState.value = "안녕 내이름은 정자이노, 탐정이죠."
+            repository.getProfile(localRepository.getUserId())
+                .onSuccess { profile ->
+                    // _nickNameState.value = profile.nickname
+                }
+                .onFailure {
+                    _profileUiEvent.emit(UiEvent.Failure(it.message))
+                }
         }
     }
 
     fun updateNickname(nickname: String){
         viewModelScope.launch {
             _nickNameState.value = nickname
-            // remote logic
+            repository.editNickname(localRepository.getUserId(), _nickNameState.value)
+                .onSuccess { profile ->
+                    _nickNameState.value = profile.nickname
+                }
+                .onFailure {
+                    _profileUiEvent.emit(UiEvent.Failure(it.message))
+                }
         }
+    }
+
+    sealed class UiEvent{
+        data class Failure(val message: String?): UiEvent()
     }
 }
