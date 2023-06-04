a1fb2a310bcaa912b790eea7411993d0e9d70b9a
jeongjaino <akgk155@gmail.com>
2023-06-04 15:55:31 +0900

    [FEATURE] #104 : edit nickname시 nickname local store 저장 구현

--- android/core/data/src/main/java/com/jaino/data/repository/user/LocalUserRepositoryImpl.kt
+++ android/core/data/src/main/java/com/jaino/data/repository/user/LocalUserRepositoryImpl.kt
@@ -6,6 +6,11 @@ import javax.inject.Inject
 class LocalUserRepositoryImpl @Inject constructor(
     private val dataStore : BeJuRyuDatastore
 ) : LocalUserRepository{
+
+    override suspend fun setNickname(nickname: String){
+        dataStore.nickName = nickname
+    }
+
     override suspend fun getNickName(): String = dataStore.nickName
 
     override suspend fun getUserId(): Long = dataStore.userId
