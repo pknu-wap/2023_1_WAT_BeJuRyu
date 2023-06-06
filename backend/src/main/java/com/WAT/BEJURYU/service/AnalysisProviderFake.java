7f9a70e03699ce5e980b2c3c3f9cf8a6bfb623c1
nuyh <hjaehyun25@gmail.com>
2023-06-07 04:09:12 +0900

    feat: Fake AI 객체 결과 랜덤 반환

--- backend/src/main/java/com/WAT/BEJURYU/service/AnalysisProviderFake.java
+++ backend/src/main/java/com/WAT/BEJURYU/service/AnalysisProviderFake.java
@@ -1,15 +1,18 @@
 package com.WAT.BEJURYU.service;
 
 import com.WAT.BEJURYU.dto.AnalysisSourceRequest;
-import com.WAT.BEJURYU.entity.Analysis;
-import com.WAT.BEJURYU.entity.Member;
 import com.WAT.BEJURYU.entity.Sentiment;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
-public class AnalysisProviderFake implements AnalysisProvider{
+public class AnalysisProviderFake implements AnalysisProvider {
     @Override
     public Sentiment analyze(AnalysisSourceRequest request) {
-        return Sentiment.HAPPY_1;
+        final Sentiment[] sentiments = Sentiment.values();
+        Arrays.sort(sentiments);
+
+        return sentiments[0];
     }
 }
