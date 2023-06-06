e666de72b76b3abc5120d34a65459bc5dc972930
nuyh <hjaehyun25@gmail.com>
2023-06-07 00:52:05 +0900

    feat: 분석 요청 응답값 변경

--- backend/src/main/java/com/WAT/BEJURYU/controller/AnalysisApiController.java
+++ backend/src/main/java/com/WAT/BEJURYU/controller/AnalysisApiController.java
@@ -3,7 +3,6 @@ package com.WAT.BEJURYU.controller;
 import com.WAT.BEJURYU.auth.config.AuthParam;
 import com.WAT.BEJURYU.auth.dto.UserId;
 import com.WAT.BEJURYU.dto.AnalysisCreatedResponse;
-import com.WAT.BEJURYU.dto.AnalysisHistory;
 import com.WAT.BEJURYU.dto.AnalysisResponse;
 import com.WAT.BEJURYU.dto.AnalysisResponses;
 import com.WAT.BEJURYU.dto.AnalysisSourceRequest;
@@ -16,9 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -36,13 +32,10 @@ public class AnalysisApiController {
     }
 
     @GetMapping
-    public ResponseEntity<List<AnalysisHistory>> findAnalyzeByUserId(@AuthParam UserId userId) {
+    public ResponseEntity<AnalysisResponses> findAnalyzeByUserId(@AuthParam UserId userId) {
         final AnalysisResponses analyze = analysisService.getAnalyze(userId.get());
-        final List<AnalysisHistory> result = analyze.getAnalyze().stream()
-                .map(AnalysisHistory::from)
-                .collect(Collectors.toList());
 
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(analyze);
     }
 
     @PostMapping("/sources")
