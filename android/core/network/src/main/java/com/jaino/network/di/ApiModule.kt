953057fd38839fb35f568729ef114c16e7c751bf
jeongjaino <akgk155@gmail.com>
2023-07-18 16:09:02 +0900

    [CHORE] #154 : json convert warning 표시하기

--- android/core/network/src/main/java/com/jaino/network/di/ApiModule.kt
+++ android/core/network/src/main/java/com/jaino/network/di/ApiModule.kt
@@ -8,6 +8,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
@@ -17,6 +18,7 @@ import retrofit2.Converter
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
+@OptIn(ExperimentalSerializationApi::class)
 @Module
 @InstallIn(SingletonComponent::class)
 object ApiModule {
