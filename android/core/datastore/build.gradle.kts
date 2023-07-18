05f965964cea0e391d57efaac6fbbb395ae72bb9
jeongjaino <akgk155@gmail.com>
2023-07-18 15:44:59 +0900

    [CHORE] #154 : datastore 및 protobuf 블록 추가

--- android/core/datastore/build.gradle.kts
+++ android/core/datastore/build.gradle.kts
@@ -2,17 +2,39 @@ plugins {
     id("com.jaino.feature")
     id("com.jaino.serialization")
     id("com.jaino.hilt")
+    alias(libs.plugins.google.protobuf)
 }
 
 android {
     namespace = "com.jaino.datastore"
 }
 
-dependencies {
-    implementation(project(":core:model"))
-    implementation(libs.bundles.kotlin)
-    implementation(libs.androidx.security)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.junit)
-    androidTestImplementation(libs.androidx.test.espresso)
+protobuf {
+    protoc {
+        artifact = libs.protobuf.protoc.get().toString()
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                register("java") {
+                    option("lite")
+                }
+                register("kotlin") {
+                    option("lite")
+                }
+            }
+        }
+    }
+
+    dependencies {
+        implementation(project(":core:model"))
+        implementation(libs.bundles.kotlin)
+        implementation(libs.bundles.datastore)
+        implementation(libs.androidx.security)
+        implementation(libs.timber)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.test.junit)
+        androidTestImplementation(libs.androidx.test.espresso)
+    }
 }
+
